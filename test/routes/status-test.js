fcc7a7aff93db4898d3708f8bec140ae2d492aac
Anders Janmyr <anders@janmyr.com>
2014-08-21 08:44:33 +0200

    Added a status route and a supertest

---
+++ test/routes/status-test.js
@@ -0,0 +1,31 @@
+var request = require('supertest');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var express = require('express');
+var routes = require('../../lib/routes/status');
+
+app = express();
+app.use('/status', routes);
+
+describe('GET /status', function() {
+    before(function() {
+        // sinon.spy(Model, 'generate');
+    });
+
+    it('responds with health good', function(done) {
+        request(app)
+            .get('/status')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res){
+                if (err) throw err;
+                expect(res.body.health).to.equal('good');
+                done();
+            });
+    });
+
+    after(function() {
+        // Model.generate.restore();
+    });
+});
+
