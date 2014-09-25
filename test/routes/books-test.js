b771309625d0ffdae8d638d9423989b5af6033dd
Anders Janmyr <anders@janmyr.com>
2014-09-25 11:27:59 +0200

    Added books routes.

---
+++ test/routes/books-test.js
@@ -0,0 +1,79 @@
+var request = require('supertest');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = require('../../lib/routes/books');
+
+var app = express();
+app.use(bodyParser.json());
+app.use('/books', router);
+
+describe('GET /books?filter=fooled', function() {
+    it('responds with matching books', function(done) {
+        request(app)
+            .get('/books?filter=fooled')
+            .end(function(err, res){
+                if (err) throw err;
+                expect(res.get('content-type')).to.match(/json/);
+                expect(res.body.length).to.equal(1);
+                expect(res.body.length).to.equal(1);
+                done();
+            });
+    });
+});
+
+describe('GET /books/geb', function() {
+    it('responds with matching book', function(done) {
+        request(app)
+            .get('/books/geb')
+            .end(function(err, res){
+                if (err) throw err;
+                expect(res.get('content-type')).to.match(/json/);
+                expect(res.body.title).to.match(/Gödel/);
+                done();
+            });
+    });
+});
+
+describe('POST /books', function() {
+    it('adds the new book', function(done) {
+        request(app)
+            .post('/books')
+            .send({author: 'fake', title: 'Fakebook swims!'})
+            .end(function(err, res){
+                if (err) throw err;
+                expect(res.get('content-type')).to.match(/json/);
+                expect(res.body).to.equal('fas');
+                done();
+            });
+    });
+});
+
+describe('PUT /books/fbr', function() {
+    it('responds with matching book', function(done) {
+        request(app)
+            .put('/books/fbr')
+            .send({title: 'Fakebook rocks!'})
+            .end(function(err, res){
+                if (err) throw err;
+                expect(res.get('content-type')).to.match(/json/);
+                expect(res.body.title).to.match(/Fakebook/);
+                done();
+            });
+    });
+});
+
+describe('DELETE /books/fbr', function() {
+    it('responds with matching book', function(done) {
+        request(app)
+            .delete('/books/fbr')
+            .end(function(err, res){
+                if (err) throw err;
+                expect(res.get('content-type')).to.match(/json/);
+                expect(res.body.title).to.match(/Fakebook/);
+                done();
+            });
+    });
+});
+
