8839b2f8dd3e6dd07d7aba76cf0348d6aa3d4e8f
Anders Janmyr <anders@janmyr.com>
2014-10-04 10:56:28 +0200

    Added some styleing and functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,32 @@
 $(function() {
-    var socket = io.connect('http://localhost');
-    console.log(socket);
-    socket.on('connect', function () {
-        console.log('Connected');
-    });
-
-
     function bookItem(book) {
         return '<li id="book-' + book.id + '">\n' +
             '<div class="title">' + book.title +  '</div>\n' +
             '<div class="title">' + book.author +  '</div>\n' +
             '</li>\n';
     }
+
+    function show(type, message) {
+        $('#message')
+            .removeClass('info error')
+            .addClass(type)
+            .text(message);
+        setTimeout(function() { $('#message').removeClass('info error'); }, 4000);
+    }
+
+    function showInfo(message) {
+        show('info', message);
+    }
+
+    function showError(message) {
+        show('error', message);
+    }
+
+    var socket = io.connect('http://localhost');
+    console.log(socket);
+    socket.on('connect', function () {
+        console.log('Connected');
+        showInfo('Connected');
+    });
+
 });
