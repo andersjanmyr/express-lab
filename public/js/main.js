ea9c7e84cc3d0cb786921dc853741f67f67e30d5
Anders Janmyr <anders@janmyr.com>
2014-10-04 10:31:11 +0200

    Added new instructions

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,12 @@ $(function() {
     socket.on('connect', function () {
         console.log('Connected');
     });
+
+
+    function bookItem(book) {
+        return '<li id="book-' + book.id + '">\n' +
+            '<div class="title">' + book.title +  '</div>\n' +
+            '<div class="title">' + book.author +  '</div>\n' +
+            '</li>\n';
+    }
 });
